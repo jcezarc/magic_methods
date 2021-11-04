e89b9e3acea9d502af26d5c4459726d9529ad5ca
Julio <jcezarc@yahoo.com.br>
2021-11-03 23:36:58 -0300

    Adicionando setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='comida',
+    version='0.2021.11.03 r23.35',
+    description='Classe para receita de comida e seus ingredientes',
+    author='Júlio Cascalles',
+    author_email='jcezarc@yahoo.com.br',
+    python_requires='>=3.6',
+)
